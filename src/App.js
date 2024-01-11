99be9b425e100e9d45b5d8510599062579278d4c
VukVerovic <vukverovic@gmail.com>
2024-01-11 21:06:55 +0100

    Project setup, UserItem, UserList and some css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Users from "./user/pages/Users";
+import NewPlaces from "./places/pages/NewPlaces";
+import { Switch } from "react-router-dom/cjs/react-router-dom.min";
 
-function App() {
-  return <h1>Let's start!</h1>;
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/users">
+          <Users />
+        </Route>
+        <Route path="/places/new">
+          <NewPlaces />
+        </Route>
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
---
+++ src/user/pages/Users.js
@@ -0,0 +1,25 @@
+import React from "react";
+import UserList from "../components/UserList";
+
+const Users = () => {
+  const dummyUsers = [
+    {
+      id: Math.random() + "",
+      name: "Pera Peric",
+      image:
+        "https://scontent-fra3-1.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=62eGHtX4ObAAX90L8K-&_nc_ht=scontent-fra3-1.xx&cb_e2o_trans=t&oh=00_AfBd4AEZk20eENy-gl7glDT1onX2mRf-D3PgBgQzdtifaQ&oe=65C7AE0E",
+      places: 2,
+    },
+    {
+      id: Math.random() + "",
+      name: "Marko Markovic",
+      image:
+        "https://scontent-fra3-1.xx.fbcdn.net/v/t1.6435-9/180978949_314228950059549_1005358403722529104_n.jpg?_nc_cat=105&ccb=1-7&_nc_sid=be3454&_nc_ohc=62eGHtX4ObAAX90L8K-&_nc_ht=scontent-fra3-1.xx&cb_e2o_trans=t&oh=00_AfBd4AEZk20eENy-gl7glDT1onX2mRf-D3PgBgQzdtifaQ&oe=65C7AE0E",
+      places: 3,
+    },
+  ];
+
+  return <UserList items={dummyUsers} />;
+};
+
+export default Users;
